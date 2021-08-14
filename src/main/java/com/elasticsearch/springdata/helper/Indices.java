fb6b4c116b42b10da17e58aee673786396e6703e
HANSAOUI_NACEUREDDINE@emsi-edu.ma <HANSAOUI_NACEUREDDINE@emsi-edu.ma>
2021-08-14 15:59:23 +0100

    Spring Data Project To Index And Search Document In ElastiSearch

---
+++ src/main/java/com/elasticsearch/springdata/helper/Indices.java
@@ -0,0 +1,7 @@
+package com.elasticsearch.springdata.helper;
+
+public final class Indices {
+
+    public static final String PERSON_INDEX = "person";
+    public static final String VEHICLE_INDEX = "vehicle";
+}
---
+++ src/main/java/com/elasticsearch/springdata/service/PersonService.java
@@ -0,0 +1,25 @@
+package com.elasticsearch.springdata.service;
+
+import com.elasticsearch.springdata.document.Person;
+import com.elasticsearch.springdata.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private final PersonRepository repository;
+
+    @Autowired
+    public PersonService(PersonRepository repository) {
+        this.repository = repository;
+    }
+
+    public void save(final Person person) {
+        repository.save(person);
+    }
+
+    public Person findById(final String id) {
+        return repository.findById(id).orElse(null);
+    }
+}
